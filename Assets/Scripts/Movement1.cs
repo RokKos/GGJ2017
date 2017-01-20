7cd612a1d3ae0c592cdd614ced5f29af54b22096
Choro11 <Choro11@bitbucket.org>
2017-01-20 22:54:47 +0100

    Movement1 test

---
+++ Assets/Scripts/Movement1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement1 : MonoBehaviour {
+
+    public float speed;
+    //public float timeSpeed;
+    private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+        newPosition = transform.position;
+        Time.timeScale = 0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        checkClick();
+        moveToPosition();
+    }
+
+    private void moveToPosition()
+    {
+        if (transform.position != newPosition)
+        {
+            Time.timeScale = 1f;
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
+        }
+        else {
+            Time.timeScale = 0f;
+            //Debug.Log("No move");
+        }
+    }
+
+    private void checkClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("klik");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Debug.Log("rayCastHit");
+                newPosition = hit.point;
+                //transform.position = newPosition;
+            }
+        }
+    }
+}
