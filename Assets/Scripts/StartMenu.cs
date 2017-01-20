a0cb5f71b2bfb434c7febd07c3ab28ae498cb5f2
Choro11 <Choro11@bitbucket.org>
2017-01-21 00:04:07 +0100

    Basic start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+
+	
+	public void onStartClicked()
+    {
+        Debug.Log("START");
+        Application.LoadLevel("TestMovement1");
+    }
+
+    public void onExitClicked()
+    {
+        Debug.Log("EXIT");
+        Application.Quit();
+    }
+}
