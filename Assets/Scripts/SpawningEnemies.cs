f792d31429ee424dbf948d4e09836a119add6447
RokKos <rocki5555@gmail.com>
2017-01-21 02:47:54 +0100

    Implementation of enemy clases and spawn rate

--- Assets/Scripts/SpawningEnemies.cs
+++ Assets/Scripts/SpawningEnemies.cs
@@ -11,76 +11,115 @@ using UnityEngine;
 public class SpawningEnemies : MonoBehaviour {
 
     [SerializeField] GameObject enemyPrefab;  // GameObject for enemie
+    [SerializeField] GameObject bulletPrefab;  // GameObject for enemie
     private GameObject[] allEnemies;  // List of all enemies
-    private Vector3[,] positionsFromTo;
-    private const int MAXENEMIESONSCENE = 100;
+    private EnemyBaseClass[] enemiesData;
+    private const int MAXENEMIESONSCENE = 1000;
+    private const float SIZEOFBOX = 10.0f;
     private int currNumberOfEnemies = 10;  // Current number enemies in scene
     private const int spawnNewEnemyInSeconds = 3;  // When new enemy spawns
     private float timePassed = 0.0f;
-    
-	
-	void Start () {
+    private const int BOUND = 20;
+    private int spodnjaIzbira = 1;
+    private int zgornjaIzbira = 2;
+
+
+    void Start () {
         timePassed = 0.0f;
+        spodnjaIzbira = 1;
+        zgornjaIzbira = 2;
         currNumberOfEnemies = 10;
+        enemiesData = new EnemyBaseClass[MAXENEMIESONSCENE];
         allEnemies = new GameObject[MAXENEMIESONSCENE];
-        positionsFromTo = new Vector3[MAXENEMIESONSCENE, 2];
         for (int i = 0; i < currNumberOfEnemies; ++i) {
-            createEnemy(i);
+            //TODO: Create advanced enemies later
+            int tip = randomPick();
+            createEnemy(i, tip);
         }
 	}
 	
 
 	void Update () {
         // Moving enemies
-        float speed = 5.0f * Time.deltaTime;
         for (int i = 0; i < currNumberOfEnemies; ++i) {
-            allEnemies[i].transform.position = Vector3.MoveTowards(allEnemies[i].transform.position, positionsFromTo[i, 1], speed);
-
-            if (Mathf.Abs(allEnemies[i].transform.position.x - positionsFromTo[i, 1].x) < 0.1f && Mathf.Abs(allEnemies[i].transform.position.y - positionsFromTo[i, 1].y) < 0.1f) {
+            allEnemies[i].transform.position = enemiesData[i].nextMove(allEnemies[i].transform.position);
+            // Checking if they are at the end
+            if (Mathf.Abs(allEnemies[i].transform.position.x - enemiesData[i].getEndPos().x) < 0.1f &&
+                Mathf.Abs(allEnemies[i].transform.position.y - enemiesData[i].getEndPos().y) < 0.1f &&
+                enemiesData[i].getType() != 4) {  // if is bullet do not instantiate again
                 // Not deleting object but rather just moving it to another starting point
-                createEnemy(i);
-            } 
+                int tip = randomPick();
+                Debug.Log(tip);
+                createEnemy(i, tip);
+            }
+
+            // Check if they can shoot
+            if (enemiesData[i].getType() == 3) {
+                /*GameObject tempBullet = (GameObject)Instantiate(bulletPrefab, allEnemies[i].transform.position,
+                    Quaternion.Euler(Quaternion.ToEulerAngles(allEnemies[i].transform.rotation)));
+
+                createEnemy(currNumberOfEnemies, 4);
+                currNumberOfEnemies++;
+                Destroy(tempBullet, 10);*/
+            }
+             
         }
 
         // Check if you can spawn new enemy
         if (timePassed > spawnNewEnemyInSeconds) {
             timePassed = 0.0f;
             // First create enemy with that number and then add because for loop goes to one less than currNumberOfEnmies
-            createEnemy(currNumberOfEnemies);
+            int tip = randomPick();
+            createEnemy(currNumberOfEnemies, tip);
             currNumberOfEnemies++;
         }
+
+        // Check if dificulty gets tuffer
+        // Tweak a little this parameters
+        if (currNumberOfEnemies > BOUND && currNumberOfEnemies < 2 * BOUND) {
+            zgornjaIzbira = 3;
+        } else if (currNumberOfEnemies > 2 * BOUND && currNumberOfEnemies < 3 * BOUND) {
+            zgornjaIzbira = 4;
+        } else if (currNumberOfEnemies > 3 * BOUND && currNumberOfEnemies < 4 * BOUND) {
+            spodnjaIzbira = 2;
+        } else if (currNumberOfEnemies > 4 * BOUND) {
+            spodnjaIzbira = 3;
+        }
+
         timePassed += Time.deltaTime;
 	}
 
-    private void createEnemy (int index) {
+    private void createEnemy (int index, int tip) {
         // Seeting of object
         float scaleOfEnemy = 1.0f;
+        float speed = 5.0f * Time.deltaTime;
         int whichSite = Random.Range(1,5);
+
         Vector3 startPos;
         Vector3 endPos;
         switch (whichSite) {
             case 1:
-                startPos = new Vector3(10.0f, Random.Range(-10.0f, 10.0f), 0.0f);
-                endPos = new Vector3(-10.0f, Random.Range(-10.0f, 10.0f), 0.0f);
+                startPos = new Vector3(SIZEOFBOX, Random.Range(-SIZEOFBOX, SIZEOFBOX), 0.0f);
+                endPos = new Vector3(-SIZEOFBOX, Random.Range(-SIZEOFBOX, SIZEOFBOX), 0.0f);
                 break;
             case 2:
-                startPos = new Vector3(Random.Range(-10.0f, 10.0f), 10.0f, 0.0f);
-                endPos = new Vector3(Random.Range(-10.0f, 10.0f), -10.0f, 0.0f);
+                startPos = new Vector3(Random.Range(-SIZEOFBOX, SIZEOFBOX), SIZEOFBOX, 0.0f);
+                endPos = new Vector3(Random.Range(-SIZEOFBOX, SIZEOFBOX), -SIZEOFBOX, 0.0f);
                 break;
 
             case 3:
-                startPos = new Vector3(Random.Range(-10.0f, 10.0f), -10.0f, 0.0f);
-                endPos = new Vector3(Random.Range(-10.0f, 10.0f), 10.0f, 0.0f);
+                startPos = new Vector3(Random.Range(-SIZEOFBOX, SIZEOFBOX), -SIZEOFBOX, 0.0f);
+                endPos = new Vector3(Random.Range(-SIZEOFBOX, SIZEOFBOX), SIZEOFBOX, 0.0f);
                 break;
 
             case 4:
-                startPos = new Vector3(-10.0f, Random.Range(-10.0f, 10.0f), 0.0f);
-                endPos = new Vector3(10.0f, Random.Range(-10.0f, 10.0f), 0.0f);
+                startPos = new Vector3(-SIZEOFBOX, Random.Range(-SIZEOFBOX, SIZEOFBOX), 0.0f);
+                endPos = new Vector3(SIZEOFBOX, Random.Range(-SIZEOFBOX, SIZEOFBOX), 0.0f);
                 break;
 
             default:
-                startPos = new Vector3(10.0f, Random.Range(-10.0f, 10.0f), 0.0f);
-                endPos = new Vector3(-10.0f, Random.Range(-10.0f, 10.0f), 0.0f);
+                startPos = new Vector3(SIZEOFBOX, Random.Range(-SIZEOFBOX, SIZEOFBOX), 0.0f);
+                endPos = new Vector3(-SIZEOFBOX, Random.Range(-SIZEOFBOX, SIZEOFBOX), 0.0f);
                 break;
         }
 
@@ -88,16 +127,27 @@ public class SpawningEnemies : MonoBehaviour {
 
         // Spawning on scene
         GameObject result;
+        EnemyBaseClass temp = new EnemyBaseClass((byte)tip, startPos, endPos, speed);
+        if (tip == 1 || tip == 4) {
+            temp = new EnemyBaseClass((byte)tip, startPos, endPos, speed);
+        } else if (tip == 2) {
+            temp = new EnemyCurveClass((byte)tip, startPos, endPos, speed, Time.time, new Vector3(0,0,0));  // TODO: Get user position
+        } else if (tip == 3) {
+            temp = new EnemyShootClass((byte)tip, startPos, endPos, speed, Time.time, new Vector3(0,0,0), 20, Time.time);  // TODO: Get user position
+        }
+
         if (allEnemies[index] == null) {
             result = (GameObject)Instantiate(enemyPrefab, startPos, Quaternion.Euler(orientationOfEnemy));
+            enemiesData[index] = temp;
         } else {
             result = allEnemies[index];
+            
+            enemiesData[index] = temp;
         }
          
         result.transform.localScale = new Vector3(scaleOfEnemy, scaleOfEnemy, 1);  // Setting scale of star
         result.name = "Enemy" + index.ToString();
-        positionsFromTo[index, 0] = startPos;
-        positionsFromTo[index, 1] = endPos;
+        
         allEnemies[index] = result;
 
     }
@@ -109,5 +159,9 @@ public class SpawningEnemies : MonoBehaviour {
             }
         }
         return 0;
+    }
+
+    private int randomPick () {
+        return Random.Range(spodnjaIzbira, zgornjaIzbira);
     }
 }
